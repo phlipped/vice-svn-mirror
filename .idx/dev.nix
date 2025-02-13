770c9a4fc96e4145b49035e20d367f99eb3e1f79
phlipped <phlipped@gmail.com>
2025-02-13 05:48:32 +0000

    Add pkgs.autoconf and pkgs.automake, which are needed to run autogen.sh

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,11 +6,8 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.autoconf
+    pkgs.automake
   ];
 
   # Sets environment variables in the workspace
